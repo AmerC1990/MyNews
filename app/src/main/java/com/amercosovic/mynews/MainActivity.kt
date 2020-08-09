9333b108c677c19c9af27b587233c0cae3b0496e
Prateek Prasad <leavethemails@gmail.com>
2020-08-09 19:50:41 +0530

    Cleanup news UI

--- app/src/main/java/com/amercosovic/mynews/MainActivity.kt
+++ app/src/main/java/com/amercosovic/mynews/MainActivity.kt
@@ -19,24 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         // Set MyNewsDataFragment to frame layout in Main Activity and pass instance of
         // top stories to fragment
-        val fragment = MyNewsDataFragment.newInstance(TOP_STORIES)
-        replaceFragment(fragment)
+        loadNews(TOP_STORIES)
 
         // When tab is selected, pass new instance to fragment and set fragment to framelayout
         tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when (tab?.text) {
                     "Top Stories" -> {
-                        val fragment = MyNewsDataFragment.newInstance(TOP_STORIES)
-                        replaceFragment(fragment)
+                        loadNews(TOP_STORIES)
                     }
                     "Most Popular" -> {
-                        val fragment = MyNewsDataFragment.newInstance(MOST_POPULAR)
-                        replaceFragment(fragment)
+                        loadNews(MOST_POPULAR)
                     }
                     "Sports" -> {
-                        val fragment = MyNewsDataFragment.newInstance(SPORTS)
-                        replaceFragment(fragment)
+                        loadNews(SPORTS)
                     }
                 }
             }
@@ -70,23 +66,33 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onResume() {
-
-        super.onResume()
-    }
-
-    private fun replaceFragment(fragment: MyNewsDataFragment) {
+    private fun loadFragment(fragment: MyNewsDataFragment) {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.frame_layout, fragment)
             commit()
         }
-        // Constants for different tabs
     }
 
-    companion object PATHS {
+    private fun loadNews(type: String) {
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.news_container)
+        if (currentFragment == null) {
+            when (type) {
+                TOP_STORIES -> loadFragment(MyNewsDataFragment.newInstance(TOP_STORIES))
+                MOST_POPULAR -> loadFragment(MyNewsDataFragment.newInstance(MOST_POPULAR))
+                SPORTS -> loadFragment(MyNewsDataFragment.newInstance(SPORTS))
+            }
+        } else {
+            (currentFragment as MyNewsDataFragment).reloadNews(type)
+        }
+    }
+
+    // Constants for different tabs
+    companion object CONSTANTS {
         const val MOST_POPULAR = "most_popular"
         const val TOP_STORIES = "top_stories"
         const val SPORTS = "sports"
+        const val API_KEY = "G9Xfi28dQn57YSw4gz11Smt0eBZumn6m"
     }
 
 }
